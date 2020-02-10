b068d804e2367d79597d92330331e6ca2705c73f
Sergey190309 <sa6702@gmail.com>
2020-02-10 11:37:46 +0300

    Header with staling has been done

--- src/App.js
+++ src/App.js
@@ -3,30 +3,19 @@ import { Switch, Route } from "react-router-dom";
 
 import './App.css';
 import ShopPage from "./pages/shop/shop.component";
-
 import HomePage from "./pages/homepage/homepage.component";
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats page</h1>
-  </div>
-)
-
-const JackersPage = () => (
-  <div>
-    <h1>Jackers Page</h1>
-  </div>
-)
+import Header from "./components/header/header.component";
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
-        <Route exact path="/hats" component={HatsPage} />
-        <Route exact path="/jackets" component={JackersPage} />
+        {/* <Route exact path="/hats" component={HatsPage} />
+        <Route exact path="/jackets" component={JackersPage} /> */}
       </Switch>
     </div>
   );
