f682adcc198092ab631e4b9fb5762ac54adab2f6
Sergey190309 <sa6702@gmail.com>
2020-02-18 11:13:40 +0300

    add and remove item functionality has been added into cart componenet

--- src/redux/cart/cart.reducer_good.js
+++ src/redux/cart/cart.reducer_good.js
@@ -1,6 +1,5 @@
-import CartActionTypes from "./cart.types";
-import { addItemToCart, removeItemFromCart } from "./cart.utils";
-
+import CartActionTypes from './cart.types';
+import { addItemToCart, removeItemFromCart } from './cart.utils';
 
 const INITIAL_STATE = {
   hidden: true,
@@ -23,7 +22,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         cartItems: removeItemFromCart(state.cartItems, action.payload)
-      };
+        };
     case CartActionTypes.CLEAR_ITEM_FROM_CART:
       return {
         ...state,
